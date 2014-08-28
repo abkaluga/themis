083a5692a50ad0702c56ab0c046b60a73dcf147e
Albert <a.b.kaluga@gmail.com>
2014-08-28 15:50:04 +0200

    Dijkstra

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,6 +1,7 @@
 
 #include <vector>
 #include <set>
+#include <stack>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>     // uint**_t
@@ -9,62 +10,55 @@ using namespace std;
  
 const uint32_t infty = 100001;
 uint32_t n,m;
-vector< pair<uint32_t,uint32_t> > adj[100000];
+vector< pair<uint32_t,uint32_t> > adj[100000];        // Graph, adj[v].first=u adj[v].second=c v is connected with u with c cost.
  
-vector<uint32_t> weight;
- 
-bool cmp(const uint32_t &a, const uint32_t &b){
-        if (weight[a] < weight[b]) return true;
-        if (weight[a] > weight[b]) return false;
-        return a<b;
+vector<uint32_t> weight;                              // cost go to the city
+
+bool cmp(uint32_t a, uint32_t b){
+    if (weight[a]<=weight[b]) return true;
+    else return false;
 }
 
- 
- 
-set<uint32_t,bool(*)(const uint32_t &a, const uint32_t &b)> mound(cmp);
+set<uint32_t,bool(*)(uint32_t,uint32_t)> mset(cmp);
  
  
 void dijkstra(uint32_t s){
-    uint32_t v, u, c;
+    uint32_t v, u, c=0;
     weight.clear();
-    weight.resize(n, infty); //
+    weight.resize(n, infty);                        // All cost are go to infinity.
  
-    weight[s] = 0;
-    mound.clear();
-    mound.insert(s);
+    weight[s] = 0;                                  // You are in s town.
+    mset.clear();
+    mset.insert(s);
  
-    while( !mound.empty()){
-        u = *(mound.begin());
-        mound.erase(mound.begin());
+    while(!mset.empty()){
+        u = *(mset.begin());
+        mset.erase(mset.begin());
  
         for (uint32_t i=0; i<adj[u].size(); ++i){
             v = adj[u][i].first;
             c = adj[u][i].second;
-            if (weight[u]+c < weight[v])
-            {
-                mound.erase(mound.find(v));
+            if (weight[u]+c < weight[v]){
                 weight[v] = weight[u]+c;
-                mound.insert(v);
+                if (mset.find(v)==mset.end()) mset.insert(v);
             }
         }
-    }
- 
- 
+    } 
 }
  
 int main(void){
-    uint32_t a,b,c, s,t;
+    uint32_t a,b,c;
  
- //   cin >> n >> m;
+    scanf("%u %u",&n,&m);
     for (uint32_t i=0; i<m; i++)    {
-   //     cin >> a >> b >> c; // c = koszt krawędzi od a do b
-        adj[a].push_back(pair<uint32_t,uint32_t>(b,c));
-        adj[b].push_back(pair<uint32_t,uint32_t>(a,c));
+        scanf("%u %u %u",&a,&b,&c);
+        adj[a-1].push_back(pair<uint32_t,uint32_t>(b-1,c));
+        adj[b-1].push_back(pair<uint32_t,uint32_t>(a-1,c));
     }
- 
-    //cin >> s >> t; // s - źródło, t - wierzchołek, do którego najkrótszej odległości szukamy
- 
-    dijkstra(s); // alg. Dijkstry wypełni całą tablicę weight[..] najkrótszymi odległościami
-    //cout << waga[t];
+    dijkstra(0);
+    for (uint32_t i = 1; i < n; ++i){
+        printf("%u ",weight[i]);
+    }
+    printf("\n");
     return 0;
 }
